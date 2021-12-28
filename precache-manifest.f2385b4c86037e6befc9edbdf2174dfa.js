e12c0b1dd0c0fc4beb377036e60526377a65d395
acepmon <dtsogtbayar123@gmail.com>
2021-12-28 14:15:51 +0800

    Updates

---
+++ precache-manifest.f2385b4c86037e6befc9edbdf2174dfa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0528288b7351042b31bd0aa0466e7b0",
+    "url": "/gyalzuul/index.html"
+  },
+  {
+    "revision": "c198c88f8f246074a775",
+    "url": "/gyalzuul/static/css/main.61139317.chunk.css"
+  },
+  {
+    "revision": "06eabe70a57fbcbea016",
+    "url": "/gyalzuul/static/js/2.552c2045.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/gyalzuul/static/js/2.552c2045.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c198c88f8f246074a775",
+    "url": "/gyalzuul/static/js/main.f2e10530.chunk.js"
+  },
+  {
+    "revision": "00a4e45d6e6f40252d2f",
+    "url": "/gyalzuul/static/js/runtime-main.09b3bdbb.js"
+  }
+]);
